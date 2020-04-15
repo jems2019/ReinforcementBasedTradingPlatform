6848ee1c400ee6e2ca4ff87a67bc3c650e498200
Jonathan Gee <jonathan.r.gee@gmail.com>
2020-04-15 16:44:30 -0700

    fixed my merge

--- API/flask_wep_app.py
+++ API/flask_wep_app.py
@@ -15,6 +15,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
+app = Flask(__name__)
+
+print('loading lstm...')    
+tf_model = load_model('./models/LSTM_model_4.h5')
+lstm_model = LSTMStockPrediction(tf_model, './models/LSTM_model_4.h5')
+
 
 print('loading ppo2...')
 ppo2_model = PPO2.load('./models/aapl_trained_model_sent_real_pred.zip')
@@ -22,9 +28,11 @@ ppo2_model = PPO2.load('./models/aapl_trained_model_sent_real_pred.zip')
 print('loading sentiment...')
 sent_crawler = SentimentCrawler()
 
+sentiment_df = pd.DataFrame()
 
+#create RL model with other pieces
+rl_model = RLModel(ppo2_model, lstm_model, sent_crawler)
 
-# rl_model.get_action('AAPL', '2020-01-01')
 
 # setup Firestore DB
 # Use a service account
@@ -45,12 +53,52 @@ def index():
     return "This is root!!!!"
 
 
+
+
+# get prediction from model in one call, should read data from firebase
+"""
+params - 
+ticker: <stock symbol>
+date: <date in form YYYY-MM-DD to get a prediction from>
+
+returns - 
+action: {'buy', 'sell', 'hold'}
+percent: percent of balance/stock to buy/sell
+"""
 @app.route('/api/get_rl_action/', methods=["GET"])
 def get_rl_action():
     ticker = str(request.args.get('ticker'))
     date = str(request.args.get('date'))
 
+    #load historical data and get range for sentiment
+    date_range = rl_model.build_history(ticker, date)
 
+    print(sentiment_df.loc[date_range])
+
+    #sentiment_df = #get from firebase with date_range
+
+    rl_action = rl_model.get_action_from_sent(ticker, date, sentiment_df.loc[date_range])
+
+    print('\n\n\n')
+    print(rl_action)
+
+    action_type = np.argmax(rl_action[0:2])
+    action_dict = {0:'buy', 1:'sell', 2:'hold'}
+    response = {
+        'action': action_dict[action_type],
+        'percent': str(rl_action[3])
+    }
+
+
+    return jsonify(response)
+
+
+
+# get prediction from model in one call, fetches sentiment on the fly
+"""
+params - 
+ticker: <stock symbol>
+date: <date in form YYYY-MM-DD to get a prediction from>
 
 returns - 
 action: {'buy', 'sell', 'hold'}
@@ -243,6 +291,38 @@ def get_text_prediction():
         return jsonify({'error': 'invalid input'})
 
     return jsonify({'you sent this': json['text']})
+
+
+
+# store sentiment data to the firebase
+"""
+params - 
+ticker: <stock symbol>
+start_date: <date in form YYYY-MM-DD>
+end_date: <date in form YYYY-MM-DD> - forms range to get sentiment from
+
+
+returns - 
+successful on finishing
+"""
+@app.route('/api/store_sentiment/', methods=["POST"])
+def store_sentiment():
+
+    ticker = str(request.args.get('ticker'))
+    start_date = str(request.args.get('start_date'))
+    end_date = str(request.args.get('end_date'))
+
+    #global sentiment_df
+    sentiment_df = sent_crawler.get_sent_from_range(ticker, start_date, end_date)
+
+    #this df has 1 column of sentiment and indexed by date
+    print(sentiment_df)
+
+    #put this df into firebase somehow
+
+    return jsonify({'successful':True})
+
+
 
 
 def automate_trade():
